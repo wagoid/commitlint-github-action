7ccafb1f5f7bb2fccef68be26aee77044e33fba7
Wagner Santos <wagnermecsantos@gmail.com>
2020-08-21 05:48:51 -0300

    ci: add release job

---
+++ .github/tasks/actionYamlUpdater.js
@@ -0,0 +1,15 @@
+const yaml = require('yaml')
+
+yaml.scalarOptions.str.fold.lineWidth = 100
+
+const versionRegex = /\d+\.\d+\.\d+/
+
+module.exports.readVersion = contents =>
+  yaml.parse(contents).runs.image.match(versionRegex)[0]
+
+module.exports.writeVersion = (contents, version) => {
+  const actionFile = yaml.parse(contents)
+  actionFile.runs.image = actionFile.runs.image.replace(versionRegex, version)
+
+  return yaml.stringify(actionFile)
+}
