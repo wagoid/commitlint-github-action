cd7f07d5ef238d2a92f064e3c590d3e8b69b8e63
Wagner Santos <7467450+wagoid@users.noreply.github.com>
2021-02-12 16:25:26 -0300

    style: ignore body-max-line-length rule for deps commits

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,27 @@
+const { maxLineLength } = require('@commitlint/ensure')
+
+const bodyMaxLineLength = 100
+
+const validateBodyMaxLengthIgnoringDeps = parsedCommit => {
+  const { type, scope, body } = parsedCommit
+  const isDepsCommit =
+    type === 'chore' && (scope === 'deps' || scope === 'deps-dev')
+
+  return [
+    isDepsCommit || !body || maxLineLength(body, bodyMaxLineLength),
+    `body's lines must not be longer than ${bodyMaxLineLength}`,
+  ]
+}
+
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  plugins: ['commitlint-plugin-function-rules'],
+  rules: {
+    'body-max-line-length': [0],
+    'function-rules/body-max-line-length': [
+      2,
+      'always',
+      validateBodyMaxLengthIgnoringDeps,
+    ],
+  },
 }
