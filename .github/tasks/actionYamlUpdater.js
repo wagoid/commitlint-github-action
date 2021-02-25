0064be9f7e17108dcd875bcb856d71e5e2c81c68
Jeroen de Bruijn <vv9239@gmail.com>
2021-02-25 18:46:28 +0100

    style: add ESLint ignore file to enable linting of dotfiles
    Closes #100.

--- .github/tasks/actionYamlUpdater.js
+++ .github/tasks/actionYamlUpdater.js
@@ -1,10 +1,11 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 const yaml = require('yaml')
 
 yaml.scalarOptions.str.fold.lineWidth = 100
 
 const versionRegex = /\d+\.\d+\.\d+/
 
-module.exports.readVersion = contents =>
+module.exports.readVersion = (contents) =>
   yaml.parse(contents).runs.image.match(versionRegex)[0]
 
 module.exports.writeVersion = (contents, version) => {
