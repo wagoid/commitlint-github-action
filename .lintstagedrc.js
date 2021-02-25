dbaa380ea6f04cc124c443b12abac24347e9354c
Jeroen de Bruijn <62570005+jdbruijn@users.noreply.github.com>
2021-02-25 13:43:32 +0100

    chore: fix invalid lintstaged configuration (#102)

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
   '*.{ts,tsx,vue,css,less,scss,html,htm,md,markdown}': 'prettier --write',
   '*.{json,yml,yaml}': ['prettier --write', () => 'npm run test'],
-  '*.{js,jsx}': ['eslint --fix', () => 'npm run test']],
+  '*.{js,jsx}': ['eslint --fix', () => 'npm run test'],
 }
