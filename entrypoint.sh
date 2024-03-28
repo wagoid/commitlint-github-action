06143b235b3cbc9b59880866cf547f9b7b52b79c
Wagner Santos <7467450+wagoid@users.noreply.github.com>
2024-03-28 16:29:46 -0300

    fix: rollback update of commitlint to support configs that use commonjs

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,13 +5,11 @@ set -e
 if [ -z "$NODE_PATH" ]; then
   export NODE_PATH=/node_modules
 else
-  export NODE_PATH="$NODE_PATH":/node_modules
+  export NODE_PATH=$NODE_PATH:/node_modules
 fi
 
 # Since actions/checkout can be setup with a different user ID, we need to set the workspace as safe inside this action
 # Info about the vunlerability: https://github.blog/2022-04-12-git-security-vulnerability-announced/
 git config --global --add safe.directory "$GITHUB_WORKSPACE"
 
-export NODE_OPTIONS="$NODE_OPTIONS --experimental-vm-modules"
-
-node /run.mjs
+node /run.js
--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,10 @@
-export default {
+module.exports = {
   // Automatically clear mock calls and instances between every test
+  // preset: 'rollup-jest',
   clearMocks: true,
   testEnvironment: '@commitlint/test-environment',
-  testMatch: [
-    '**/__tests__/**/*.?(m)[jt]s?(x)',
-    '**/?(*.)+(spec|test).?(m)[tj]s?(x)',
-  ],
   transform: {
     '\\.[jt]sx?$': 'babel-jest',
   },
+  transformIgnorePatterns: ['node_modules/(?!dargs)'],
 }
